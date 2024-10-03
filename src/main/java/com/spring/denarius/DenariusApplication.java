5479d59e98dc84fc4b701051852975bbdbc3aaa3
insivam <88207216+insivam@users.noreply.github.com>
2024-10-02 23:07:54 -0300

    first commit

---
+++ src/main/java/com/spring/denarius/DenariusApplication.java
@@ -0,0 +1,14 @@
+package com.spring.denarius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenariusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenariusApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/spring/denarius/DenariusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.denarius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenariusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
